d51fddff36b695e83f0008fe33bde1925cd0eb9e
Stacie Ruiz <s_lynn.r@outlook.com>
2023-02-14 08:51:58 -0600

    commit

---
+++ src/test/java/com/ruiz/Spark/SparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruiz.Spark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
